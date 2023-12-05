1c8764785278a9beb2ae8a8f4352ae78c5bd365a
aitanax <100472097@alumnos.uc3m.es>
2023-12-05 01:44:54 +0100

    restricciones 4 y 5

--- CSParking.py
+++ CSParking.py
@@ -25,4 +25,25 @@ def procesar_archivo(path):
 
 # ------------------------------ FUNCIÓN PARA RESTRICCIÓN 4: TSU>TNU ---------------------------------
 
+def restriccion_aparcado_por_delante(v_tsu, v_tnu):
+    # Un TSU no puede tener aparcado por delante, en su misma fila, a ningún otro vehículo excepto si es también de tipo TSU
+    return v_tsu[0] != v_tnu[0] or v_tsu[1] >= v_tnu[1]  # Se cambia >= por <=
+
 # ------------------------------ FUNCIÓN PARA RESTRICCIÓN 5: MANIOBRABILIDAD ---------------------------------
+def restriccion_maniobrabilidad(v1, v2, v3):
+        
+        # Condicion tres vehiculos seguidos:
+        if abs(v1[0] - v2[0]) == 1 and abs(v1[0] - v3[0]) == 1 and v1[1] == v2[1] == v3[1]:
+                return False
+        # Condicion fila 1:
+        if v1[0] == 1:  
+                # Derecha
+                if (v1[0] + 1 == v2[0] and v1[1]==v2[1]) or (v1[0] + 1 == v3[0] and v1[1]==v3[1]):
+                    return False
+        # Condicion ultima fila:
+        if v1[0] == filas: 
+                # Izquierda
+                if (v1[0] - 1 == v2[0] and v1[1]==v2[1]) or (v1[0] - 1 == v3[0] and v1[1]==v3[1]):
+                    return False
+
+        return True
