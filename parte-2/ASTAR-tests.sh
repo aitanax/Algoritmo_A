aed910749c6fcb578cc44814175cfe59335c4854
aitanax <100472097@alumnos.uc3m.es>
2023-12-15 22:49:17 +0100

    TODO

--- parte-2/ASTAR-tests.sh
+++ parte-2/ASTAR-tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Script para ejecutar casos de prueba para ASTARTraslados.py
 
-python3 ASTARTraslados.py ./ASTAR-tests/mapa04.csv 1
-python3 ASTARTraslados.py ./ASTAR-tests/mapa04.csv 2
-python3 ASTARTraslados.py ./ASTAR-tests/mapa04.csv 3
-python3 ASTARTraslados.py ./ASTAR-tests/mapa04.csv 4
-python3 ASTARTraslados.py ./ASTAR-tests/mapa02.csv 1
-python3 ASTARTraslados.py ./ASTAR-tests/mapa02.csv 2
-python3 ASTARTraslados.py ./ASTAR-tests/mapa02.csv 3
-python3 ASTARTraslados.py ./ASTAR-tests/mapa02.csv 4
-python3 ASTARTraslados.py ./ASTAR-tests/mapa01.csv 1
-python3 ASTARTraslados.py ./ASTAR-tests/mapa01.csv 2
-python3 ASTARTraslados.py ./ASTAR-tests/mapa01.csv 3
-python3 ASTARTraslados.py ./ASTAR-tests/mapa01.csv 4
+python3 ASTARTranslados.py ./ASTAR-tests/mapa04.csv 1
+python3 ASTARTranslados.py ./ASTAR-tests/mapa04.csv 2
+python3 ASTARTranslados.py ./ASTAR-tests/mapa04.csv 3
+python3 ASTARTranslados.py ./ASTAR-tests/mapa04.csv 4
+python3 ASTARTranslados.py ./ASTAR-tests/mapa02.csv 1
+python3 ASTARTranslados.py ./ASTAR-tests/mapa02.csv 2
+python3 ASTARTranslados.py ./ASTAR-tests/mapa02.csv 3
+python3 ASTARTranslados.py ./ASTAR-tests/mapa02.csv 4
+python3 ASTARTranslados.py ./ASTAR-tests/mapa01.csv 1
+python3 ASTARTranslados.py ./ASTAR-tests/mapa01.csv 2
+python3 ASTARTranslados.py ./ASTAR-tests/mapa01.csv 3
+python3 ASTARTranslados.py ./ASTAR-tests/mapa01.csv 4
+python3 ASTARTranslados.py ./ASTAR-tests/mapa03.csv 3
